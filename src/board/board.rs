492d99bcc30e9de916afce1e3f21a5bfb0bdcf75
sSlugs <ethan@trakker.ca>
2025-04-23 21:46:47 -0700

    feature: added board manipulation funcs

--- src/board/board.rs
+++ src/board/board.rs
@@ -13,11 +13,11 @@ pub const BITMASKS: [u64; 64] = {
 
 // Enums
 #[repr(usize)]
-#[derive(Copy, Clone,PartialEq)]
+#[derive(Copy, Clone,PartialEq,Debug)]
 pub enum Colour { White = 0, Black = 1 }
 
 #[repr(usize)]
-#[derive(Copy, Clone,PartialEq)]
+#[derive(Copy, Clone,PartialEq,Debug)]
 pub enum PieceType {
     Pawn   = 0,
     Knight = 1,
@@ -63,14 +63,39 @@ impl Board { // Board manipulation functions
     }
 
     #[inline(always)]
-    pub fn clear_square(&mut self,sq: u64) { //clears square on board
+    pub fn clear_square(&mut self,sq: usize) { //clears square on board
+        let old = self.mailbox[sq];
 
+        if old == 0xFF { return; }
+
+        let nm       = !BITMASKS[sq];
+        self.pieces[old as usize / 6][old as usize % 6] ^= nm;
+        self.occupancy[old as usize / 6] ^= nm;
+        self.occupancy[2]               ^= nm;
+
+        self.mailbox[sq] = 0xFF;
     }
 
-    //#[inline(always)]
-    //pub fn piece_at_square(&mut self,sq: u64) -> Option<(PieceType,Colour)> { // returns piecetype and colour of square of board
-    //    return;
-    //}
+    #[inline(always)]
+    pub fn piece_at_square(&mut self,sq: usize) -> Option<(PieceType,Colour)> { // returns piecetype and colour of square of board
+        let square = self.mailbox[sq];
+
+        if square == 0xFF {return None;}
+
+        let colour = if square < 6 { Colour::White } else { Colour::Black };
+        let piece = match square % 6 {
+            0 => PieceType::Pawn,
+            1 => PieceType::Knight,
+            2 => PieceType::Bishop,
+            3 => PieceType::Rook,
+            4 => PieceType::Queen,
+            5 => PieceType::King,
+            _ => unreachable!(),
+        };
+
+        return Some((piece,colour));
+    }
+
 }
 
 impl Board { // Init functions
@@ -95,7 +120,16 @@ impl Board { // Init functions
                         0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
                         0b11111111_11111111_00000000_00000000_00000000_00000000_11111111_11111111],
 
-            mailbox: [0xFF; 64],
+            mailbox: [
+                3, 1, 2, 4, 5, 2, 1, 3,
+                0, 0, 0, 0, 0, 0, 0, 0,
+                0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+                0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+                0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF, 
+                0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,
+                6, 6, 6, 6, 6, 6, 6, 6,
+                9, 7, 8, 10,11, 8, 7, 9,
+            ],
         }
     }
 
@@ -108,31 +142,27 @@ impl Board { // Init functions
     }
 }
 
-impl Board { // UI
+impl Board { // UI / UX
     pub fn print(&self) {
-        // Iterate ranks 7 down to 0
         for rank in (0..8).rev() {
-            // Print rank number (optional)
             print!("{} ", rank + 1);
             for file in 0..8 {
                 let sq = rank * 8 + file;
                 let glyph = match self.mailbox[sq] {
                     0xFF => '.',
                     idx => {
-                        // Decode color and piece type
                         let idx = idx as usize;
                         let color = if idx < 6 { Colour::White } else { Colour::Black };
                         let pt = idx % 6;
                         let ch = match pt {
-                            0 => 'P', // Pawn
-                            1 => 'N', // Knight
-                            2 => 'B', // Bishop
-                            3 => 'R', // Rook
-                            4 => 'Q', // Queen
-                            5 => 'K', // King
+                            0 => 'P', 
+                            1 => 'N', 
+                            2 => 'B', 
+                            3 => 'R', 
+                            4 => 'Q', 
+                            5 => 'K',
                             _ => '?',
                         };
-                        // Lowercase for black
                         if color == Colour::Black {
                             ch.to_ascii_lowercase()
                         } else {
@@ -144,7 +174,6 @@ impl Board { // UI
             }
             println!();
         }
-        // Print file letters
         print!("  ");
         for file in 0..8 {
             let letter = (b'a' + file as u8) as char;
@@ -152,26 +181,4 @@ impl Board { // UI
         }
         println!();
     }
-}
-
-// bit manipulation 
-#[inline(always)]
-pub fn set_bit(bb: &mut u64, sq: u8) {
-    // safe, bounds-checked
-    *bb |= BITMASKS[sq as usize];
-}
-
-#[inline(always)]
-pub fn clear_bit(bb: &mut u64, sq: u8) {
-    *bb &= !BITMASKS[sq as usize];
-}
-
-#[inline(always)]
-pub fn toggle_bit(bb: &mut u64, sq: u8) {
-    *bb ^= BITMASKS[sq as usize];
-}
-
-#[inline(always)]
-pub fn is_set(bb: u64, sq: u8) -> bool {
-    bb & BITMASKS[sq as usize] != 0
 }
